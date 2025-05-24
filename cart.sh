a443bd3d2c4eaa1ee9c323dc4d582b2c6fe94b8d
maheswari <mahi.sgs22@gmail.com>
2025-05-24 22:22:24 +0530

    roboshop1 created

--- cart.sh
+++ cart.sh
@@ -1,80 +1,10 @@
 #!/bin/bash
 
+source ./common.sh
+app_name=cart
+check_root
+app_setup
+nodejs_Setup
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-START_TIME=$(date +%s)
-LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.logs
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "Script started execution at :: $(date)"
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
-then
-    echo "$R ERROR:: Please run the script with root user $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running the script with root user" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling the nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling the nodejs"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "installing the nodejs"
-
-id roboshop
-if [ $? -ne 0 ]
-then 
-    useradd --system --home /app --sh /sbin/nologin --comment "Roboshop user" roboshop
-    VALIDATE $? "Creating the Roboshop User"
-else
-    echo "user is already created"
-fi
-
-mkdir -p /app
-VALIDATE $? "Creating the /app directory"
-
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading the cart file"
-
-rm -rf /app/*
-cd /app
-unzip /tmp/cart.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping the cart file"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing the dependencies"
-
-cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
-
-systemctl daemon-reload
-systemctl enable cart &>>$LOG_FILE
-systemctl start cart &>>$LOG_FILE
-VALIDATE $? "Starting the cart service"
-
-
-
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME-$START_TIME))
-echo "Script executed in $TOTAL_TIME seconds"
+system_setup
+print_value
--- redis.sh
+++ redis.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
-USERID=$(id -u)
-START_TIME=$(date +%s)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.logs"
-
-mkdir -p $LOGS_FOLDER
-echo "Script started execution at:: $(date)"
-if [ $USERID -ne 0 ]
-then
-    echo "$R ERROR:: Please run the script with root user $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running the script with root user" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo "$2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo "$2 is .... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+source ./common.sh
+app_name=redis
+check_root
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling the redis"
@@ -45,12 +19,7 @@ systemctl restart redis &>>$LOG_FILE
 systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling the redis"
 
-
 systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting the redis"
 
-
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME-$START_TIME))
-echo "Script executed in $TOTAL_TIME seconds"
+print_value

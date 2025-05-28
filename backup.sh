43ee7338b44e094d36dcd9cdabc2d05759689ce8
maheswari <mahi.sgs22@gmail.com>
2025-05-28 20:19:09 +0530

    backup file

--- backup.sh
+++ backup.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-USERID=$(id -u)
-LOGS_FOLDER="/var/logs/shellscript-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
 
+mkdir -p LOGS_FOLDER
+
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: Please run the script with root access" | tee -a $LOGS_FILE
     exit 1
 else
-    echo "You are Running with root access"
+    echo "You are running the script with Root user" | tee -a $LOGS_FILE
 fi
 
 echo "Script started executing at $(date)"
 
 USAGE(){
-     echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
-     exit 1
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
 }
+
 if [ $# -lt 2 ]
 then
     USAGE
@@ -33,36 +36,33 @@ fi
 
 if [ ! -d $SOURCE_DIR ]
 then
-    echo -e "$R Source Directory $SOURCE_DIR does not exist. Please check $N"
-    exit 1
+    echo -e "$R Source Directory $SOURCE_DIR Does not Exists. Please Check $N"
 fi
 
 if [ ! -d $DEST_DIR ]
 then
-    echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
-    exit 1
+    echo -e "$R Destination Directory $DEST_DIR Does not Exists. Please Check $N"
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-if [ ! -z "$FILES" ]
+if [ ! -z $FILES ] # here -z stands for empty file but we are using ! -z non empty file
 then
     echo "Files to zip are: $FILES"
     TIME_STAMP=$(date +%F-%H-%M-%S)
-    ZIP_FILE="$DEST_DIR/app-logs-$TIME_STAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ $ZIP_FILE
-    if [ -f $ZIP_FILE ]
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    $FILES | zip -@ $ZIP_FILE
+    if [ -z $ZIP_FILE ]
     then
         echo "Successfully created zip file"
-        while IFS= read -r $filepath
-        do 
-            echo "Deleting file: $filepath" | tee -a $LOG_FILE
-            rm -rf $filepath
+        while IFS= read -r line
+        do
+            echo "Deleting Files: $line" | tee -a $LOG_FILE
+            rm -rf $line
         done <<< $FILES
         echo -e "Log files older than $DAYS from source directory removed ... $G SUCCESS $N"
     else
         echo -e "Zip file creation ... $R FAILURE $N"
         exit 1
-    fi
 else
     echo -e "No other files found older than 14 days ... $Y SKIPPING $N"
 fi
